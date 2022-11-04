abea1af4205e70971370f9acc66c24a29a5667a8
Kevin Agreda <36802620+KevinAgreda245@users.noreply.github.com>
2022-11-03 20:25:41 -0600

    Creación de modelos

--- Administrador/models.py
+++ Administrador/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+class Usuario(AbstractUser):
+    documento = models.CharField(max_length=9,null=False,unique=True)
+    nacimiento = models.DateField(null=False)            
+    class Meta:
+        db_table = "usuario"
+class TipoPrenda(models.Model):
+    nombre = models.CharField(max_length=60,null=False,unique=True)
+    class Meta:
+        db_table = "tipoPrenda"
+class Talla(models.Model):
+    nombre = models.CharField(max_length=60,null=False)
+    tipoPrenda = models.ForeignKey(TipoPrenda,on_delete=models.CASCADE,null=False)
+    class Meta:
+        db_table = "talla"
+        unique_together = ('nombre', 'tipoPrenda')
+class Prenda(models.Model):
+    nombre = models.CharField(max_length=60,null=False)
+    descripcion = models.CharField(max_length=120,null=False)
+    costo = models.FloatField(null=False)
+    precioVenta = models.FloatField(null=False)
+    visibilidad = models.IntegerField(null=False)
+    urlFoto = models.CharField(max_length=400,null=False)
+    tipoPrenda = models.ForeignKey(TipoPrenda,on_delete=models.CASCADE,null=False)
+    class Meta:
+        db_table = "prenda"
+        unique_together = ('nombre', 'descripcion','urlFoto','tipoPrenda')
+class Inventario(models.Model): 
+    cantidad = models.IntegerField(null=False),
+    prenda = models.ForeignKey(Prenda,on_delete=models.CASCADE,null=False)
+    talla = models.ForeignKey(Talla,on_delete=models.CASCADE,null=False)
+    class Meta:
+        db_table = "inventario"
+        unique_together = ('prenda','talla')
--- Cliente/models.py
+++ Cliente/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from Administrador.models import Inventario,Usuario
 
-# Create your models here.
+class Orden(models.Model):
+    fecha = models.DateTimeField(default=timezone.now)
+    cliente = models.ForeignKey(Usuario,on_delete=models.CASCADE,null=False)
+
+    class Meta:
+        db_table = "orden"
+
+class DetalleDeOrden(models.Model):
+    costo = models.FloatField(null=False)
+    precio = models.FloatField(null=False)
+    cantidad = models.FloatField(null=False)
+    inventario = models.ForeignKey(Inventario,on_delete=models.CASCADE,null=False)
+    orden = models.ForeignKey(Orden,on_delete=models.CASCADE,null=False)
+    class Meta:
+        db_table = "detalleOrden"
+        unique_together = ('orden', 'inventario')
