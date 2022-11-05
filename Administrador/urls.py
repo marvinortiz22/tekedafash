146a5259434696f01c7ae82684fff37f75069370
Kevin Agreda <36802620+KevinAgreda245@users.noreply.github.com>
2022-11-04 18:27:06 -0600

    Pantalla para gestionar productos

--- Administrador/urls.py
+++ Administrador/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "dashboard")
+    path('/', views.index, name = "dashboard"),
+    path('/gestionarProducto', views.gestionarProducto, name = "gestionarProducto")
 ]
--- Administrador/views.py
+++ Administrador/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'Administrador/index.html')
+
+def gestionarProducto(request):
+    return render(request, 'Administrador/gestionarProducto.html')
