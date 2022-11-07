f4bf397badfbfd8c9d3308ad98bb2d83968bcb2d
Kevin Agreda <36802620+KevinAgreda245@users.noreply.github.com>
2022-11-07 09:15:22 -0600

    Incorporación de Datatable

---
+++ static/js/init.js
@@ -0,0 +1,24 @@
+function initTable(id,registros) {
+    $(id).DataTable({
+      "language": {
+        "decimal": "",
+        "emptyTable": "No hay servicios registrados",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ "+registros,
+        "infoEmpty": "Mostrando 0 a 0 de 0 "+registros,
+        "infoFiltered": "(Filtrado de _MAX_ total " + registros +")",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ "+registros,
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    }
+    });
+  }
