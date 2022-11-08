caf16588e81fb225ae61fa73788b42a9a9b3a2ff
Diego Perla <pc20004@ues.edu.sv>
2022-11-07 19:33:27 -0600

    Calendario pantalla de inicio Admin

--- Administrador/views.py
+++ Administrador/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from datetime import date
+from calendar import HTMLCalendar
+
 
 def index(request):
-    return render(request, 'Administrador/index.html')
+    fecha_actual = date.today()
+    cal = HTMLCalendar().formatmonth(fecha_actual.year, fecha_actual.month)
+    return render(request, 'Administrador/index.html', {"cal": cal})
+
 
 def gestionarProducto(request):
     return render(request, 'Administrador/gestionarProducto.html')
