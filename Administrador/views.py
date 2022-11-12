84f88b10f35cbb75c3084631346c3dfcc14cc16d
Diego Perla <pc20004@ues.edu.sv>
2022-11-11 23:54:41 -0600

    funcion para construir calendario

--- Administrador/views.py
+++ Administrador/views.py
@@ -1,22 +1,17 @@
 from django.shortcuts import render
+from Cliente.models import *
 from django.shortcuts import redirect
 from datetime import date
 from calendar import HTMLCalendar
 import calendar
 
-from Cliente.models import *
-
 
 def index(request):
-    fecha_actual = date.today()
-    cal = calendar.LocaleHTMLCalendar(firstweekday=6, locale='es').formatmonth(
-        fecha_actual.year, fecha_actual.month)
-    calCurrent = cambiarCalendario(
-        cal, 'cellpadding="0"', 'cellpadding="6px"')
-    calCurrent = cambiarCalendario(
-        calCurrent, '>%i<' % fecha_actual.day, 'class="" bgcolor="#​008374" ><font color="#FFFFFF"><b>%i</b></font><' % fecha_actual.day)
-
-    return render(request, 'Administrador/index.html', {"cal": calCurrent})
+    calCurrent = buildCalendar(date.today())
+    currentYear = date.today().year
+    label = Orden.objects.filter(fecha__range=(
+        date(2020, 1, 1), date(2022, 11, 11)))
+    return render(request, 'Administrador/index.html', {"cal": calCurrent, "fechas": label, "año": currentYear})
 
 
 def gestionarProducto(request):
@@ -49,3 +44,13 @@ def editarAdmin(request, id):
 def cambiarCalendario(objeto, cambio, cambios):
     objeto = objeto.replace(cambio, cambios)
     return objeto
+
+
+def buildCalendar(fecha_actual):
+    cal = calendar.LocaleHTMLCalendar(firstweekday=6, locale='es').formatmonth(
+        fecha_actual.year, fecha_actual.month)
+    calCurrent = cambiarCalendario(
+        cal, 'cellpadding="0"', 'cellpadding="6px"')
+    calCurrent = cambiarCalendario(
+        calCurrent, '>%i<' % fecha_actual.day, 'class="" bgcolor="#​008374" ><font color="#FFFFFF"><b>%i</b></font><' % fecha_actual.day)
+    return calCurrent
