b228976bcfe601f5a93bf1b584e1191ff81bd5e7
Diego Perla <pc20004@ues.edu.sv>
2022-11-23 15:33:34 -0600

    Página Quienes somos

--- Cliente/urls.py
+++ Cliente/urls.py
@@ -3,29 +3,35 @@ from django.contrib.auth import views as auth_vistas
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "inicio"),
-    path('iniciarSesion', views.iniciarSesion, name = "iniciarSesion"),
-    path('cerrarSesion', views.cerrarSesion, name = "cerrarSesion"),
-    
-    #Reestablecer contraseña
-    path('reestablecerContraseña', auth_vistas.PasswordResetView.as_view(template_name = "Cliente/reestablecerContraseña/reestablecerContraseña.html"), name = "password_reset"),
-    path('reestablecerContraseñaEnviado', auth_vistas.PasswordResetDoneView.as_view(template_name = "Cliente/reestablecerContraseña/reestablecerContraseñaEnviado.html"), name = "password_reset_done"),
-    path('establecerContraseña/<uidb64>/<token>/', auth_vistas.PasswordResetConfirmView.as_view(template_name = "Cliente/reestablecerContraseña/establecerContraseñaNueva.html"), name = "password_reset_confirm"),
-    path('contraseñaEstablecida', auth_vistas.PasswordResetCompleteView.as_view(template_name = "Cliente/reestablecerContraseña/reestablecerContraseñaCompletado.html"), name = "password_reset_complete"),
-    #Mis Compras
-    path('misCompras', views.misCompras, name = "misCompras"),
-    path('detalledemiCompra/<id>', views.detalledemiCompra, name = "detalledemiCompra"),
-    
-    #Registro
+    path('', views.index, name="inicio"),
+    path('iniciarSesion', views.iniciarSesion, name="iniciarSesion"),
+    path('cerrarSesion', views.cerrarSesion, name="cerrarSesion"),
+    path('quieneSomos', views.quieneSomos, name="quieneSomos"),
+
+    # Reestablecer contraseña
+    path('reestablecerContraseña', auth_vistas.PasswordResetView.as_view(
+        template_name="Cliente/reestablecerContraseña/reestablecerContraseña.html"), name="password_reset"),
+    path('reestablecerContraseñaEnviado', auth_vistas.PasswordResetDoneView.as_view(
+        template_name="Cliente/reestablecerContraseña/reestablecerContraseñaEnviado.html"), name="password_reset_done"),
+    path('establecerContraseña/<uidb64>/<token>/', auth_vistas.PasswordResetConfirmView.as_view(
+        template_name="Cliente/reestablecerContraseña/establecerContraseñaNueva.html"), name="password_reset_confirm"),
+    path('contraseñaEstablecida', auth_vistas.PasswordResetCompleteView.as_view(
+        template_name="Cliente/reestablecerContraseña/reestablecerContraseñaCompletado.html"), name="password_reset_complete"),
+    # Mis Compras
+    path('misCompras', views.misCompras, name="misCompras"),
+    path('detalledemiCompra/<id>', views.detalledemiCompra,
+         name="detalledemiCompra"),
+
+    # Registro
     path('registrarse/', views.registrarse),
     path('registrarUsuario', views.regisUsuario,),
-    
-    #Productos
+
+    # Productos
     path('productos/', views.productos),
     path('productos/detalles/<int:id>', views.detalleProducto),
-    path('miCarrito', views.miCarrito, name = "carrito"),
-    #----------
-    path('editarPerfil',views.editarPerfil,name="editar perfil"),
-    path('Perfil/',views.Perfil,name="Perfil"),
-    path('cambiarContraseña/',views.cambiarContraseña,name="cambiar contraseña"),
+    path('miCarrito', views.miCarrito, name="carrito"),
+    # ----------
+    path('editarPerfil', views.editarPerfil, name="editar perfil"),
+    path('Perfil/', views.Perfil, name="Perfil"),
+    path('cambiarContraseña/', views.cambiarContraseña, name="cambiar contraseña"),
 ]
--- Cliente/views.py
+++ Cliente/views.py
@@ -2,14 +2,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.db.models import Sum,F
-from Cliente.decorators import * 
+from django.db.models import Sum, F
+from Cliente.decorators import *
 from .models import *
-from Administrador.models import Usuario, Prenda, Talla 
+from Administrador.models import Usuario, Prenda, Talla
 import re
 from django.core.paginator import Paginator
 from django.contrib.auth.forms import PasswordChangeForm
 from .forms import PerfilForm
+
 
 def index(request):
     request.session['carrito'] = []
@@ -19,43 +20,51 @@ def index(request):
 def miCarrito(request):
     return render(request, 'Cliente/carritoCompras.html')
 
+
 @usuarioAutenticado
 def iniciarSesion(request):
     if request.method == 'POST':
-            nombreUsuario = request.POST.get('usuario')
-            contraseña = request.POST.get('contraseña')
-            usuario = authenticate(request, username = nombreUsuario, password = contraseña)
-            if usuario is not None:
-                login(request, usuario)
-                if (usuario.is_staff):
-                    return redirect('/dashboard')
-                else:
-                    return redirect('/') 
+        nombreUsuario = request.POST.get('usuario')
+        contraseña = request.POST.get('contraseña')
+        usuario = authenticate(
+            request, username=nombreUsuario, password=contraseña)
+        if usuario is not None:
+            login(request, usuario)
+            if (usuario.is_staff):
+                return redirect('/dashboard')
             else:
-                messages.error(request, "¡Usuario o contraseña incorrectas!")
-                return render(request,'Cliente/iniciarSesion.html' )
+                return redirect('/')
+        else:
+            messages.error(request, "¡Usuario o contraseña incorrectas!")
+            return render(request, 'Cliente/iniciarSesion.html')
     return render(request, 'Cliente/iniciarSesion.html')
+
 
 @login_required(login_url='iniciarSesion')
 def cerrarSesion(request):
     logout(request)
     return redirect('iniciarSesion')
 
-def misCompras(request):
-    ordenes=DetalleDeOrden.objects.filter(orden__cliente_id = request.user.id).values('orden_id','orden__fecha').annotate(total = Sum(F('precio') * F('cantidad')))
-    contexto={"ordenes":ordenes}
-    return render(request, 'Cliente/misCompras.html',contexto)
 
-def detalledemiCompra(request,id):
-    orden=get_object_or_404(Orden,id=id,cliente_id = request.user.id)
-    ventas=DetalleDeOrden.objects.filter(orden=orden)
-    monto=0
+def misCompras(request):
+    ordenes = DetalleDeOrden.objects.filter(orden__cliente_id=request.user.id).values(
+        'orden_id', 'orden__fecha').annotate(total=Sum(F('precio') * F('cantidad')))
+    contexto = {"ordenes": ordenes}
+    return render(request, 'Cliente/misCompras.html', contexto)
+
+
+def detalledemiCompra(request, id):
+    orden = get_object_or_404(Orden, id=id, cliente_id=request.user.id)
+    ventas = DetalleDeOrden.objects.filter(orden=orden)
+    monto = 0
     for venta in ventas:
-        monto+=venta.precio*venta.cantidad
-    return render(request, 'Cliente/detalledemiCompra.html',{"ventas":ventas,"orden":orden,"monto":monto})
+        monto += venta.precio*venta.cantidad
+    return render(request, 'Cliente/detalledemiCompra.html', {"ventas": ventas, "orden": orden, "monto": monto})
+
 
 def registrarse(request):
     return render(request, 'Cliente/registrarUsuario.html')
+
 
 def regisUsuario(request):
     nombre = request.POST['nombre']
@@ -69,14 +78,16 @@ def regisUsuario(request):
     expr = re.compile('\d{9}')
     ob = expr.match(dui)
     if (contra == contra2 and ob):
-        nueuser = Usuario.objects.create(password = contra, username = user, first_name = nombre, last_name = apellido, email = correo, nacimiento = fechaNaci, documento = dui)
-        return redirect('inicio')            
+        nueuser = Usuario.objects.create(password=contra, username=user, first_name=nombre,
+                                         last_name=apellido, email=correo, nacimiento=fechaNaci, documento=dui)
+        return redirect('inicio')
     else:
         if(contra != contra2):
             messages.error(request, "Las contraseñas no coinciden")
         if not(bool(ob)):
             messages.error(request, "Digite un DUI valido")
         return render(request, 'Cliente/registrarUsuario.html')
+
 
 def productos(request):
     products = Prenda.objects.all()
@@ -90,15 +101,16 @@ def productos(request):
     }
     return render(request, 'Cliente/productos.html', context)
 
+
 def detalleProducto(request, id):
     prenda = Prenda.objects.get(id=id)
     tallas = Talla.objects.all()
-    return render(request,'Cliente/detalleProducto.html', {'prenda': prenda, 'tallas': tallas})
+    return render(request, 'Cliente/detalleProducto.html', {'prenda': prenda, 'tallas': tallas})
 
 
 def editarPerfil(request):
-    if request.method=="POST":
-        form=PerfilForm(request.POST, instance=request.user)
+    if request.method == "POST":
+        form = PerfilForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect('Perfil')
@@ -108,21 +120,27 @@ def editarPerfil(request):
             template = "Administrador/editarPerfil.html"
         else:
             template = "Cliente/editarPerfil.html"
-        return render(request,template, {"form":form})
+        return render(request, template, {"form": form})
+
 
 def Perfil(request):
     if request.user.is_staff == 1:
         template = "Administrador/Perfil.html"
     else:
         template = "Cliente/Perfil.html"
-    return render(request,template,{"usuario":request.user})
+    return render(request, template, {"usuario": request.user})
+
 
 def cambiarContraseña(request):
-    if request.method=="POST":
-        form =PasswordChangeForm(user=request.user,data=request.POST)
+    if request.method == "POST":
+        form = PasswordChangeForm(user=request.user, data=request.POST)
         if form.is_valid():
             form.save()
             return redirect('iniciarSesion')
     else:
-        form =PasswordChangeForm(user=request.user)
-    return render(request,'Cliente/cambiarContraseña.html',{"form":form})
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'Cliente/cambiarContraseña.html', {"form": form})
+
+
+def quieneSomos(request):
+    return render(request, 'Cliente/quieneSomos.html')
